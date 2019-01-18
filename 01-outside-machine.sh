3be61e34a29148b75f4a9a990bd2c839012976c8
Fabricio C Zuardi <fabricio@fabricio.org>
2019-01-18 05:32:02 -0200

    Update 01-outside-machine.sh

--- 01-outside-machine.sh
+++ 01-outside-machine.sh
@@ -7,5 +7,5 @@ ssh-copy-id rock64@kim.local
 kitty +kitten ssh rock64@kim.local
 
 # edit ssh config to add an alias to the long onion hostname
-# user torsocks ssh to access the onion ssh server
+# use torsocks ssh to access the onion ssh server
 
