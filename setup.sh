9068582c857893031e953f887b1d78220fa9df12
Fabricio C Zuardi <fabricio@fabricio.org>
2019-01-18 01:31:54 -0200

    initial setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# change default user password
+passwd
+
+# name machine after Kim Wexler
+sudo hostname kim
+
+# generate an identity for this machine
+ssh-keygen -t ed25519
+
+# download zuardistros to get useful partials
+cd ~/
+git clone git@github.com:fczuardi/zuardistros.git
+
+# enable multicast (to access via ssh with kim.local instead of the ip address)
+cd ~/zuardistros/partials
+cat begin.sh packages_multicast.sh end.sh |sed -e 's/apt/sudo apt/g'|bash
+
+exit
+
+# run kitty +kitten ssh rock64@kim.local from another machine with the kitty terminal to have the correct .terminfo installed
